b021a683cf8f7b8cad798d55800fdcdff56de762
Jake Garcia <bballwiz4@gmail.com>
2020-02-23 23:57:21 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,7 @@ end
 def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  if array.detect?(cheese_types) != nil
+  if array.detect(cheese_types) != nil
     cheese = array.detect?(cheese_types)
     return cheese
   else
