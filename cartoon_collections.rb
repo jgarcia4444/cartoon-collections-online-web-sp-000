d42b848e93ff5654f6a754fa83770e71297bd2fd
Jake Garcia <bballwiz4@gmail.com>
2020-02-23 23:57:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,13 @@ def long_planeteer_calls(calls)# code an argument here
   calls.any? {|call| call.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  if array.detect?(cheese_types) != nil
+    cheese = array.detect?(cheese_types)
+    return cheese
+  else
+    return nil
+  end
 end
