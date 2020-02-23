15b287cb3ab4ea97ada231143101c9cbb7161f66
Jake Garcia <bballwiz4@gmail.com>
2020-02-23 23:49:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 def roll_call_dwarves(dwarf_names)# code an argument here
   # Your code here
-
+  dwarf_names.each_with_index {|name, i| puts "#{i + 1}. #{name}"}
 end
 
 def summon_captain_planet# code an argument here
